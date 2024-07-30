c8c5f3f97971be66dd5c323e632d8ec6062ca6da
f00z <fayozxontolibov95@gmail.com>
2024-07-30 11:09:20 +0500

    start

--- Travil/models.py
+++ Travil/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Index(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField(max_length=100)
+    photos = models.ImageField(upload_to='media/', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}, {self.age}, {self.photos}'
--- Travil/views.py
+++ Travil/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Index
 
-# Create your views here.
+
+def index(request):
+    context = {
+        'index': Index.objects.all()
+    }
+    return render(request, 'index.html', context)
